d85aeba55b94f965721e3885d90ad41967ca9c3b
I860997 <I860997@SLEN00643745A.amer.global.corp.sap>
2018-01-16 10:11:18 -0200

    construtores

--- src/negocio/Bloco.java
+++ src/negocio/Bloco.java
@@ -2,4 +2,48 @@ package negocio;
 
 public class Bloco {
 
+	int numero;
+	Especialidade[] especialidade;	
+	Andar[] andar;
+	
+	
+	// construtor
+	public Bloco(int numero) {
+		this.numero = numero;
+	}
+	
+	public Bloco() {
+		
+	}
+	
+	// getters and setters
+	int getNumero() {
+		return numero;
+	}
+	
+	void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	// metodos
+	public void inserirPaciente(Paciente paciente) {
+		
+	}
+	
+	public void realocarPaciente(Paciente paciente) {
+		
+	}
+	
+	public void liberarPaciente(Paciente paciente) {
+		
+	}
+	
+	public void liberarAndar(Andar andar) {
+		
+	}
+	
+	public void excluirAndar(Andar andar) {
+		
+	}
+	
 }
--- src/negocio/BlocoQuatroAndares.java
+++ src/negocio/BlocoQuatroAndares.java
@@ -1,5 +1,14 @@
 package negocio;
 
-public class BlocoQuatroAndares {
+public class BlocoQuatroAndares extends Bloco{
+	
+	private static final int N_ANDARES = 4;
+	private static final int N_ESPECIALIDADES = 2;
+	
+	public BlocoQuatroAndares(int numero) {
+		super(numero);
+		this.especialidade = new Especialidade[N_ESPECIALIDADES];
+		this.andar = new Andar[N_ANDARES];
+	}
 
 }
