52622e50e9dd08a7370d23cd4e18db911fe125fa
I860997 <I860997@SLEN00643745A.amer.global.corp.sap>
2018-01-18 10:48:25 -0200

    Singleton

--- src/negocio/Hospital.java
+++ src/negocio/Hospital.java
@@ -1,5 +1,22 @@
 package negocio;
 
 public class Hospital {
+	
+	public static Hospital hospital;
+	
+	private Hospital() {
+		// aqui os dados iniciais são carregados
+		
+		// carregar valores do banco e etc..
+	}
+	
+	public static Hospital getInstance() {
+		if(hospital == null) {
+			hospital = new Hospital();
+		}
+		return hospital;
+	}
 
+	
+	
 }
